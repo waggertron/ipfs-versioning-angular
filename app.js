1fbd3679baa2365b64c8f18afa223d4bacad9d40
Weylin Wagnon <weylinwagnon@gmail.com>
2016-10-17 22:59:07 -0700

    Project service renders correctly, projectCtrl displays correct view

--- app.js
+++ app.js
@@ -2,27 +2,41 @@ angular.module('myApp', ['ui.ace'])
   .config(function ($sceProvider) {
     $sceProvider.enabled(false);
   })
+  //shoot, maybe this should be a factory
   .service('ProjectService', ['$http', '$q', function ($http, $q) {
     this.projectArrayLength = () => this.projectArray.length - 1;
-    this.selectedVersion = () => this.newestVersion;
+    this.publishedArrayLength = () => this.publishedProjectVersions().length - 1;
+    this.publishedProjectVersions = () => {
+      return this.projectArray.filter((version) => {
+        return version.publish
+      });
+    }
+    this.fileChangedVersions = () => {
+      return this.projectArray.filter((version) => {
+        return !version.publish
+      });
+    }
+
     this.currentUrl = () => this.selectedVersion.url;
     this.currentFile = () => this.selectedVersion.changed;
+    this.currentFileType = () => this.currentFile()
     this.changeSelectedVersion = (index) => {
       //will need to change to account for unpublished projects
-      this.selectedVersion = this.projectArray[this.projectArrayLength() - index];
+      this.selectedVersion = this.publishedProjectVersions()[this.publishedArrayLength() - index];
       console.log('this.selctedversion: ', this.selectedVersion);
-      this.currentUrl = this.selectedVersion.url;
+      // this.currentUrl = this.selectedVersion.url;
       console.log('this.currenturl: ', this.currentUrl);
-      this.currentFile = this.selectedVersion.changed;
+      // this.currentFile = this.selectedVersion.changed;
       console.log('this.currentFile: ', this.currentFile);
     }
     this.getContentUrl = () => {
-      return this.currentUrl.slice(0, this.currentUrl.length - 1) + this.currentFile;
+      return this.currentUrl() + this.currentFile();
     }
     this.data = testData
     this.publishedProject = 'Dispersion'
     this.projectArray = this.data[this.publishedProject];
     this.newestVersion = this.projectArray[this.projectArrayLength()];
+    this.selectedVersion = this.newestVersion;
 
     // this.editorContent = this.currentProjectVersion;
 
@@ -30,6 +44,7 @@ angular.module('myApp', ['ui.ace'])
   .controller('ProjectController', ['$http', 'ProjectService', function ($http, ProjectService) {
     const self = this;
     self.projectArray = ProjectService.projectArray;
+    self.publishedVersions = ProjectService.publishedProjectVersions;
     self.selectedVersion = ProjectService.selectedVersion;
     self.currentFile = ProjectService.currentFile;
     self.currentUrl = ProjectService.currentUrl;
@@ -64,4 +79,14 @@ angular.module('myApp', ['ui.ace'])
       }
       self.lastProjectIndex = index;
     }
-  }]);
+  }])
+  //TODO: render editor from file version, implement save  
+  //if content is a image, just show it, hide editor
+  //figure out a compare function maybe findPreviousVersion();
+  //should this be a factory? my gut says yes
+  .controller('FileController', ['$http', 'ProjectService', function ($http, ProjectService) {
+    const self = this;
+    self.fileVersions = ProjectService.fileChangedVersions; //function
+
+
+  }])
